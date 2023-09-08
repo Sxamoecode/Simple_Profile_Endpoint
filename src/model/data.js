b5ae8a93b369134276da9d968dea8584af60ee82
Sxamoecode <samuelayandare@gmail.com>
2023-09-08 14:21:48 +0100

    modified

--- src/model/data.js
+++ src/model/data.js
@@ -1,14 +1,21 @@
 
 const { slack_name, track } = '';
 const current_day = new Date().toLocaleString('en-us', { weekday: 'long' });
-const utc_time = new Date().toISOString();
+const now = new Date();
+// const offsetMinutes = 2;
+
+// Calculate the adjusted time by adding or subtracting minutes
+now.setMinutes(now.getUTCMinutes());
+// Format the date in "yyyy-MM-ddTHH:mm:ssZ" format
+const todays_time = now.toISOString().slice(0, 19) + "Z";
+
 const github_file_url = 'https://github.com/Sxamoecode/Simple_Profile_Endpoint/src/server.js';
 const github_repo_url = 'https://github.com/Sxamoecode/Simple_Profile_Endpoint';
 
 exports.data = {
     slack_name,
     current_day,
-    utc_time,
+    todays_time,
     track,
     github_file_url,
     github_repo_url,
